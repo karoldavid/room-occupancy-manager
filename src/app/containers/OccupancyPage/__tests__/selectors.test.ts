6d2fcb2679f0f08cdf173a0ac4134eca3ab0d583
karoldavid <k.zyskowski@live.com>
2020-06-13 14:20:42 +0200

    test: add unit test for revenue room selector

--- src/app/containers/OccupancyPage/__tests__/selectors.test.ts
+++ src/app/containers/OccupancyPage/__tests__/selectors.test.ts
@@ -2,8 +2,9 @@ import * as selectors from '../selectors';
 import { RootState } from 'types';
 import { GuestsErrorType } from '../types';
 import { initialState } from '../slice';
+import data from '../mock/guests.json';
 
-describe('GithubRepoForm selectors', () => {
+describe('OccupancyPage selectors', () => {
   let state: RootState;
   beforeEach(() => {
     state = {
@@ -19,19 +20,63 @@ describe('GithubRepoForm selectors', () => {
     };
   });
 
-  it('should select error', () => {
-    const error = GuestsErrorType.RESPONSE_ERROR;
-    state = {
-      occupancy: { ...initialState, error: error },
-    };
-    expect(selectors.selectError(state)).toEqual(error);
+  describe('selectError', () => {
+    it('should select error', () => {
+      const error = GuestsErrorType.RESPONSE_ERROR;
+      state = {
+        occupancy: { ...initialState, error: error },
+      };
+      expect(selectors.selectError(state)).toEqual(error);
+    });
   });
 
-  it('should select loading', () => {
-    const loading = true;
-    state = {
-      occupancy: { ...initialState, loading: loading },
-    };
-    expect(selectors.selectLoading(state)).toEqual(loading);
+  describe('selectLoading', () => {
+    it('should select loading', () => {
+      const loading = true;
+      state = {
+        occupancy: { ...initialState, loading: loading },
+      };
+      expect(selectors.selectLoading(state)).toEqual(loading);
+    });
+  });
+
+  describe('selectRevenueRoom', () => {
+    it('should return object with correct properties', () => {
+      const expectedProperties = [
+        'premium.usage',
+        'premium.total',
+        'premium.currency',
+        'economy.usage',
+        'economy.total',
+        'economy.currency',
+      ];
+      state = {
+        occupancy: {
+          ...initialState,
+          availableRooms: {
+            premium: 3,
+            economy: 3,
+          },
+          guests: [...data],
+        },
+      };
+
+      const expectedResult = {
+        premium: {
+          usage: 3,
+          total: 738,
+          currency: 'EURO',
+        },
+        economy: {
+          usage: 3,
+          total: 167,
+          currency: 'EURO',
+        },
+      };
+
+      expectedProperties.forEach((name, index) => {
+        expect(selectors.selectRevenueRoom(state)).toHaveProperty(name);
+      });
+    });
   });
 });
